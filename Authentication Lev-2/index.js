4120b54e1e716ee14645e246a1d14e8876ee7c36
Darrehan <darrehanrasool@gmail.com>
2024-04-16 20:13:39 +0530

    Dar Rehan Rasool
    sycomusiclover

--- Authentication Lev-2/index.js
+++ Authentication Lev-2/index.js
@@ -14,7 +14,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "authentication",
-  password: "postgressql9090",
+  password: "",
   port: 5432,
 });
 db.connect();
